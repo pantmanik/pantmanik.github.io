355da9672fb688f1f9b7854bc3670a7126abf366
pantmanik <pantmanik@gmail.com>
2018-04-29 21:07:03 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'These are the contents of Article One',
+			'author' : 'Manik Pant',
+			'create_date' : '29-04-2018'
+		},
+		{
+			'id' : 2,
+			'title' : 'Article Two',
+			'body' : 'These are the contents of Article Two',
+			'author' : 'Someone else',
+			'create_date' : '29-04-2018'
+		},
+		{
+			'id' : 3,
+			'title' : 'Article Three',
+			'body' : 'These are the contents of Article Three',
+			'author' : 'Someone else 2',
+			'create_date' : '29-04-2018'
+		}
+	]
+	return articles
